e6551f39296dfb3146674e8dfc391f76aba6f235
AlvaroJoseBaranoski <alvarojosebaranoski01@gmail.com>
2021-04-13 17:21:40 -0300

    Updates test.php

--- test.php
+++ test.php
@@ -1,11 +1,12 @@
 <?php
 
-$pmu = "eficiencia";
-$time_w = 30;
+$pmu = "cabine";
+$time_w = 60;
 $sample_freq = 5;
+$order = 10;
 
 // Execute the python script with the JSON data
-$results = shell_exec("python C:\Users\alvar\Desktop\IC-COMMP\ic-commp\\startup.py $pmu $time_w $sample_freq");
+$results = shell_exec("python C:\Users\alvar\Desktop\IC-COMMP\Yule-Walker\YW-backend\\startup.py $pmu $time_w $sample_freq $order");
 
 $data_results = json_decode($results, true);
 
