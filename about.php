fdafb11b2c3060f13a8cb99d0e0e209a294bcebe
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2021-06-08 19:04:40 -0300

    updates about the project and thanks text

--- about.php
+++ about.php
@@ -25,13 +25,15 @@
 
   <section id="about">
     <div class="container">
-      <h2 class="mt-4">About the project</h2>
-      <p>Real-time monitoring of the power grid has become increasingly viable due to the
-        advances in phasor measurement units (PMUs) technology, allowing system operators to analyze
-        a whole set of different events and dynamics present in the system. One particularly interesting
-        dynamic observed via PMU data is electromechanical oscillations, which pose a real threat to
-        power system stability. This website is part of a study with the objective of developing a web environment designed to estimate electromechanical modes present in the distribution grid based on real-time data from µPMU units. Those µPMUs are located at different campuses of the Universidade Federal do Paraná (UFPR). To achieve the mode estimation, the well-known Welch and Yule-Walker
-        algorithms are used. More details about this project can be found <a href="https://professorgustavo.weebly.com/pmu-based-real-time-power-systems-monitoring.html" target="_blank">here</a>.</p>
+      <h2 class="mt-4">PMU based real-time monitoring of power system dynamics</h2>
+      <p> ​
+        The main goal of this project is to investigate new methods and algorithms for PMU/ µPMU data analysis aiming to estimate power system oscillation modes.
+        The results are useful to support decisions regarding power system stability (transient and small signals) and found application to both in the Generation/Transmission or the Distribution context.<br>
+
+        Some methods and algorithms have several tunning parameters which should be wisely selected for an adequate result.
+        So, for a better understanding of the whole problem, this web environment has been designed to estimate electromechanical modes present in the distribution grid based on real-time data from µPMU units. Those µPMU are located at different campuses of the Universidade Federal do Paraná (UFPR). In this first version, we are testing the well-known Welch and Yule-Walker algorithms, which have parallel basic properties.<br>
+        More details about this project can be found <a href="https://professorgustavo.weebly.com/pmu-based-real-time-power-systems-monitoring.html" target="_blank">here</a>.
+      </p>
       <br />
       <br />
       <h3>Team</h3>
@@ -65,7 +67,7 @@
       </table>
       <br />
       <h3>Acknowledgements</h3>
-      <p>This study was supported by ANEEL’s R&D Program through COPEL’s PD 2866-0470-2017 project and also by the Coordenação de Aperfeiçoamento de Pessoal de Nível Superior Brasil (CAPES) Finance Code 001.</p>
+      <p>This research was supported by ANEEL’s R&D Program through COPEL’s PD 2866-0470-2017 project.</p>
     </div>
   </section>
 
