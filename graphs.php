f260822ebdfba012420719634e496151dc0b9a9b
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-06-29 17:08:49 -0300

    Adicionando outros prints

--- graphs.php
+++ graphs.php
@@ -34,7 +34,15 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
 function startup($pmu, $time_w, $sample_freq, $order, $filter_lower, $filter_higher, $outlier_constant, $view)
 {
 
-	print_r("executando função de startup");
+	print_r("executando função de startup \n");
+	print_r($pmu);
+	print_r($time_w);
+	print_r($sample_freq);
+	print_r($order);
+	print_r($filter_lower);
+	print_r($filter_higher);
+	print_r($outlier_constant);
+	print_r($view);
 
 	// Execute the python script with the JSON data
 	$results = shell_exec("/opt/ic-commp/bin/python3 /opt/yulewalker/main/startup.py $pmu $time_w $sample_freq $order $filter_lower $filter_higher $outlier_constant $view");
