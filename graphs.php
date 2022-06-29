51fdc85ac80e6e648b9024c44848c7c48daa6d11
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-06-29 17:30:28 -0300

    Mudando variável do server - agora vai

--- graphs.php
+++ graphs.php
@@ -53,8 +53,8 @@ function startup($pmu, $time_w, $sample_freq, $order, $filter_lower, $filter_hig
 	// print_r("\n");
 
 	// Execute the python script with the JSON data
-	// $results = shell_exec("/opt/ic-commp/bin/python3 /opt/yulewalker/main/startup.py $pmu $time_w $sample_freq $order $filter_lower $filter_higher $outlier_constant $view");
-	$results = shell_exec("D:\Alvaro\Faculdade\TCC\Source\ic-commp-yw-backend\\venv\Scripts\python.exe D:\Alvaro\Faculdade\TCC\Source\ic-commp-yw-backend\main\startup.py $pmu $time_w $sample_freq $order $filter_lower $filter_higher $outlier_constant $view");
+	$results = shell_exec("/opt/ic-commp/bin/python3 /opt/yulewalker/main/startup.py $pmu $time_w $sample_freq $order $filter_lower $filter_higher $outlier_constant $view");
+	// $results = shell_exec("D:\Alvaro\Faculdade\TCC\Source\ic-commp-yw-backend\\venv\Scripts\python.exe D:\Alvaro\Faculdade\TCC\Source\ic-commp-yw-backend\main\startup.py $pmu $time_w $sample_freq $order $filter_lower $filter_higher $outlier_constant $view");
 
 	// print_r("imprimindo resultado do comando: \n");
 	// sleep(2);
